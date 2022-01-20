9cd687326a7f17367bf79018e5a337c2146c7561
Danilo Miyamura <danilo.miyamura@gmail.com>
2022-01-19 21:17:17 -0300

    - Conexao com banco de dados mysql e criacao de tabelas

---
+++ src/main/java/br/com/financecontrol/budget/domain/model/Despesas.java
@@ -0,0 +1,27 @@
+package br.com.financecontrol.budget.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Despesas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    String descricao;
+
+    @Column(nullable = false)
+    BigDecimal valor;
+
+    @Column(nullable = false)
+    Date data;
+}
---
+++ src/main/java/br/com/financecontrol/budget/domain/model/Receitas.java
@@ -0,0 +1,29 @@
+package br.com.financecontrol.budget.domain.model;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Receitas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false)
+    String descricao;
+
+    @Column(nullable = false)
+    BigDecimal valor;
+
+    @Column(nullable = false)
+    Date data;
+
+}
