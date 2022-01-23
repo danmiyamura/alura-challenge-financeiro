135b59fd54a41ee64db0bca1bff9f1b4dfa54da1
Danilo Miyamura <danilo.miyamura@gmail.com>
2022-01-22 23:44:46 -0300

    - Detalhemento receita pelo id

--- src/main/java/br/com/financecontrol/budget/api/controller/ReceitaController.java
+++ src/main/java/br/com/financecontrol/budget/api/controller/ReceitaController.java
@@ -4,6 +4,7 @@ import br.com.financecontrol.budget.domain.model.Receita;
 import br.com.financecontrol.budget.domain.service.CadastroReceitaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class ReceitaController {
     @GetMapping
     public List<Receita> listar(){
         return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Receita buscar(@PathVariable Long id){
+        return service.findById(id);
     }
 }
