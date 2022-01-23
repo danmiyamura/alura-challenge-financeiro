0f0ef575b1cc376c20672bac53be2b9d6c13721b
Danilo Miyamura <danilo.miyamura@gmail.com>
2022-01-22 23:52:28 -0300

    - Endpoint buscar  despesa

--- src/main/java/br/com/financecontrol/budget/api/controller/DespesaController.java
+++ src/main/java/br/com/financecontrol/budget/api/controller/DespesaController.java
@@ -4,6 +4,7 @@ import br.com.financecontrol.budget.domain.model.Despesa;
 import br.com.financecontrol.budget.domain.service.CadastroDespesaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class DespesaController {
     @GetMapping
     public List<Despesa> listar(){
         return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Despesa busca(@PathVariable Long id){
+        return service.findById(id);
     }
 }
